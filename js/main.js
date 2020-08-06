abdc0e855f7899f841bb51d5f1bfdc08ac552dde
Web Publishing <59594342+konkuk-project@users.noreply.github.com>
2020-08-07 01:23:35 +0900

    자바스트립트 navbar
    navbar 투명 상태 -> 스크롤시 고정

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+    if(window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar__dark');
+    } else {
+        navbar.classList.remove('navbar__dark');
+    }
+})
