5e57508d40327a4e4eadb6847b4b4a566cafa273
Web Publishing <59594342+konkuk-project@users.noreply.github.com>
2020-08-07 02:18:30 +0900

    scrollIntoView 함수 만들기
    html data link 추가하고 전달하고자 하는 정보 id 담아서 연결

--- js/main.js
+++ js/main.js
@@ -19,7 +19,16 @@ navbarMenu.addEventListener("click", (event) => {
     if(link == null) {
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior : "smooth"});
+    scrollIntoView(link);
 })
+
+//Hand Scrolling when tapping on contact button
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", (event) => {
+    scrollIntoView("#contact");
+})
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
